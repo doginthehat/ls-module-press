46c188295a741d335f889f7dbddcf2635aaaa64c
Eric Muyser <eric@muyser.com>
2011-08-03 16:32:53 -0700

    add  action.

--- classes/press_actions.php
+++ classes/press_actions.php
@@ -1,6 +1,17 @@
 <?
 
 	class Press_Actions extends Cms_ActionScope {
+		public function article() {
+			$this->data['article'] = null;
+
+			$slug = $this->request_param(0);
+			
+			if(!$slug)
+				return;
+			
+			$this->data['article'] = Press_Article::create()->find_by_slug($slug);
+		}
+		
 		public function articles() {
 			$this->data['articles'] = Press_Article::create()->where('is_enabled=1')->find_all();
 		}
